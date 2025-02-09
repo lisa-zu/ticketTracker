edfc1fd65d4242b7466eae10f7db25691b1cdd5b
Lisa <lisa.lz.zuchowski@gmail.com>
2025-02-09 12:30:31 +0100

    removes obsolete settings

--- pages/settings.vue
+++ pages/settings.vue
@@ -26,60 +26,6 @@ export default {
 
 <template>
   <div class="flex flex-col gap-4 h-full w-dvw justify-center items-center">
-    <UFormGroup
-      class="w-2/4"
-      label="Jira Hostname"
-      required
-    >
-      <div class="flex flex-row w-full gap-4">
-        <UInput class="flex-1"
-                color="white"
-                variant="outline"
-                v-model="userStore.jiraHostName"
-                size="lg"
-        ></UInput>
-      </div>
-    </UFormGroup>
-    <UFormGroup
-        class="w-2/4"
-        label="Jira E-Mail-Adresse"
-        required
-    >
-      <div class="flex flex-row w-full gap-4">
-        <UInput class="flex-1"
-                color="white"
-                variant="outline"
-                v-model="userStore.jiraUserEmailAddress"
-                size="lg"
-        ></UInput>
-      </div>
-    </UFormGroup>
-    <UFormGroup
-        class="w-2/4"
-        label="Jira API Token"
-        required
-    >
-      <div class="flex flex-row w-full gap-4">
-        <UInput class="flex-1"
-                color="white"
-                variant="outline"
-                :type="apiTokenInputType"
-                v-model="userStore.jiraUserAPIToken"
-                size="lg"
-                :ui="{ icon: { trailing: { pointer: '' } } }"
-        >
-          <template #trailing>
-            <UButton
-                :icon="apiTokenInputIcon"
-                size="sm"
-                color="primary"
-                square
-                variant="ghost"
-                @click="toggleAPITokenInputType()"
-            />
-          </template>
-        </UInput>
-      </div>
-    </UFormGroup>
+
   </div>
 </template>
