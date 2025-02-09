2e805552385a6e5f96f1320badb56983458dee56
Lisa <lisa.lz.zuchowski@gmail.com>
2025-02-09 14:19:12 +0100

    TT-3 requests the available cloud ids when handling callback

--- server/api/oauth/callback.ts
+++ server/api/oauth/callback.ts
@@ -1,19 +1,28 @@
-import requestToken from "~/services/jira_api";
+import { requestToken, requestCloudIds } from "~/services/jira_api";
 
 export default defineEventHandler(async (event) => {
     try {
         const query = getQuery(event);
         const code = query.code as string;
+        let token: string = '';
         // request the access token based on the authorization code
         await requestToken(code)
-            .then((token) => {
-                if (token) {
-                    setCookie(event, "jira_token", token, { httpOnly: true, secure: true })
-                    return sendRedirect(event, '/dashboard')
+            .then((_token) => {
+                if (_token) {
+                    token = _token;
+                    setCookie(event, "jira_token", _token, { httpOnly: true, secure: true })
                 } else {
                     console.error('no token provided')
                 }
             })
+        await requestCloudIds(token)
+            .then((_cloudIds) => {
+                if (_cloudIds) {
+                    let cloudIdStrings: string[] = _cloudIds.map(item => item.id)
+                    setCookie(event, "cloud_ids", cloudIdStrings.toString(), { httpOnly: true, secure: true })
+                }
+            })
+        return sendRedirect(event, '/settings')
     } catch (error) {
         console.error('OAuth Callback Error:', error);
         return sendRedirect(event, '/error')
